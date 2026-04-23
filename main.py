682a1150e3110194449f7dbfc0345db79058bc81
Gugalomi=geo <iva290386@gmail.com>
2026-04-23 23:17:39 +0400

    add POST request to create a new pixel

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 
 USERNAME = "professor079"
 TOKEN = "hkjasdhjkh"
-
+GRAPH_ID = "graph1"
 
 
 pixela_endpoint = "https://pixe.la/v1/users"
@@ -19,7 +19,7 @@ user_params = {
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 
 graph_config = {
-    "id": "graph1",
+    "id": GRAPH_ID,
     "name": "Cycling graph",
     "unit": "Km",
     "type": "float",
@@ -30,14 +30,17 @@ headers = {
    "X-USER-TOKEN": TOKEN
 }
 
-response = requests.post(
-    url=graph_endpoint,
-    json=graph_config,
-    headers=headers)
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
 
+create_pixel_endpoint = f"{graph_endpoint}/{GRAPH_ID}"
 
+pixel_params = {
+    "date": "20260 223",
+    "quantity": "31.25",
+}
 
-
+response = requests.post(url=create_pixel_endpoint, json=pixel_params, headers=headers)
+print(response.text)
 
 
 
